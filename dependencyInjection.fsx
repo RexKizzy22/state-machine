e57e10c2d744f01c72ad86b09b785404c2c774f8
Kizito <kizitoinegbenose@gmail.com>
2022-05-06 17:17:55 +0100

    dependency injection

---
+++ dependencyInjection.fsx
@@ -0,0 +1,22 @@
+module DependencyInjection =
+
+    type Application = Application
+    type Ids = Ids
+    type Deposit = Deposit
+    type CheckingAccount = CheckingAccount 
+
+    let validateApplication (application: Application) =
+        true 
+        
+    let validateIds (ids: Ids) =
+        true 
+
+    let openCheckingAccount validateApplication validateIds (application: Application) (ids: Ids) (deposit: Deposit) =
+        // verify that application is complete
+        // verify that ids are visual
+        // verify that deposit amount is sufficient
+
+        // if everything is okay, then return the checkingAccount
+        if not (application |> validateApplication) then None 
+        elif not (ids |> validateIds) then None 
+        else Some CheckingAccount
--- stateMachine.fsx
+++ stateMachine.fsx
@@ -1,28 +1,5 @@
 namespace StateMachine 
 
-module DependencyInjection =
-
-    type Application = Application
-    type Ids = Ids
-    type Deposit = Deposit
-    type CheckingAccount = CheckingAccount 
-
-    let validateApplication (application: Application) =
-        true 
-        
-    let validateIds (ids: Ids) =
-        true 
-
-    let openCheckingAccount validateApplication validateIds (application: Application) (ids: Ids) (deposit: Deposit) =
-        // verify that application is complete
-        // verify that ids are visual
-        // verify that deposit amount is sufficient
-
-        // if everything is okay, then return the checkingAccount
-        if not (application |> validateApplication) then None 
-        elif not (ids |> validateIds) then None 
-        else Some CheckingAccount
-
 module Automobile =
     // state machines define rules that control how states change.
     // Given a current state X, a state machine will define what states X can change to 
