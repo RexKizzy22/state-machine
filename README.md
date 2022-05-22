660ce0e8a496f4ee3c0fcfa553a6310129544547
Kizito <kizitoinegbenose@gmail.com>
2022-05-22 01:59:25 +0100

    combinators

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # State Machine  
 
 A documentation of idiomatic F# code capturing several major F# constructs in
-different domains. Concepts captures include:
+different domains. Concepts captured include:
 
 - Domain modelling with algebraic data types.
 - Making illegal states unrepresentable.
@@ -9,5 +9,6 @@ different domains. Concepts captures include:
 - Actor model of concurrency using the MailboxProcessor.
 - Railway-oriented programming
 - Unit testing with NUnit
+- Combinators
 
 Files with shared domain logic are denoted with a shared `namespace`
--- game.fsx
+++ game.fsx
@@ -4,7 +4,7 @@ namespace Game
 
 type Details = 
     { Name: string
-      Description: string }
+        Description: string }
 
 type Item = 
     { Details: Details }
@@ -18,24 +18,25 @@ type Exit =
 
 and Room = 
     { Id: RoomId
-      Details: Details
-      Items: Details list 
-      Exits: Exits }
+    Details: Details
+    Items: Details list 
+    Exits: Exits }
 
 and Exits = 
     { North: Exit 
-      South: Exit  
-      East: Exit 
-      West: Exit }
+    South: Exit  
+    East: Exit 
+    West: Exit }
 
 type Player = 
     { Details: Details
-      Location: RoomId
-      Inventory: Item list }
+    Location: RoomId
+    Inventory: Item list }
 
 type World = 
     { Rooms: Map<RoomId, Room>
-      Player: Player }
+    Player: Player }
+
 
 module FunctionalGame = 
 
@@ -90,27 +91,27 @@ module FunctionalGame =
                         East = NoExit None
                         West = NoExit None }}
 
-                { Id = RoomId "west1"
-                Details = 
-                    { Name = "A cozy room"
-                        Description = "This room seems very cozy, as if someone had made a home here.  Various personal belongings are strewn about."}
-                Items = [ key ]
-                Exits =
-                    { North = NoExit None
-                        South = NoExit None
-                        East = PassableExit ("You see a doorway back to the lit room.", RoomId "center")
-                        West = NoExit None }}
+        { Id = RoomId "west1"
+        Details = 
+            { Name = "A cozy room"
+                Description = "This room seems very cozy, as if someone had made a home here.  Various personal belongings are strewn about."}
+        Items = [ key ]
+        Exits =
+            { North = NoExit None
+                South = NoExit None
+                East = PassableExit ("You see a doorway back to the lit room.", RoomId "center")
+                West = NoExit None }}
 
-                { Id = RoomId "east1"
-                Details = 
-                    { Name = "An open meadow"
-                        Description = "You are in an open meadow.  The sun is bright and it takes some time for your eyes to adjust."}
-                Items = []
-                Exits =
-                    { North = NoExit None
-                        South = NoExit None
-                        East = NoExit None
-                        West = PassableExit ("You see stone doorway to the west.  Why would you want to go back there?", RoomId "center") }}
+        { Id = RoomId "east1"
+        Details = 
+            { Name = "An open meadow"
+                Description = "You are in an open meadow.  The sun is bright and it takes some time for your eyes to adjust."}
+        Items = []
+        Exits =
+            { North = NoExit None
+                South = NoExit None
+                East = NoExit None
+                West = PassableExit ("You see stone doorway to the west.  Why would you want to go back there?", RoomId "center") }}
     ]
 
     let player =
@@ -123,7 +124,7 @@ module FunctionalGame =
             allRooms
             |> Seq.map (fun room -> (room.Id, room))
             |> Map.ofSeq
-          Player = player}
+          Player = player }
 
 
 
@@ -207,7 +208,7 @@ module FunctionalGame =
     |> displayResult
 
 
-   // ******** Concurrent State Machine for Aspect Game ******** //
+   // ******** Concurrent Event Loop for the Game Engine ******** //
 
     type GameEvent =
         | UpdateState of (World -> Result<World, string>)
@@ -230,8 +231,10 @@ module FunctionalGame =
                     async {
                         let! eventMsg = inbox.Receive()
                         match eventMsg with
-                        | UpdateState updateFunc -> return! innerLoop (applyUpdate updateFunc worldState)
-                        | ResetState newState -> return! innerLoop newState
+                        | UpdateState updateFunc -> 
+                            return! innerLoop (applyUpdate updateFunc worldState)
+                        | ResetState newState -> 
+                            return! innerLoop newState
                         | EndGameLoop -> return ()
                     }
                     
@@ -276,4 +279,110 @@ module FunctionalGame =
     gameEngine.ResetState(gameWorld)
 
     playerController.Post("Stop")
+
+
+
+
+// *********************** Command Parsing ********************* // 
+
+module CommandParser = 
+
+    type Parser<'a> = Parser of (char list -> Result<'a * char list, string>)
+
+    let runParser parser inputChars =
+        let (Parser parserFunc) = parser
+        parserFunc inputChars
+
+    let expectChar expectedChar =
+        let innerParser inputChars =
+            match inputChars with
+            | c :: remainingChars -> 
+                if c = expectedChar then Success (c, remainingChars)
+                else Failure (sprintf "Expected '%c', got '%c'" expectedChar c)
+            | [] ->
+                Failure (sprintf "Expected '%c', reached end of input" expectedChar)
+        
+        Parser innerParser
+
+    let stringToCharList str =
+        List.ofSeq str
+
+    let orParse parser1 parser2 =
+        let innerParser inputChars =
+            match runParser parser1 inputChars with
+            | Success result -> Success result
+            | Failure _ -> runParser parser2 inputChars
+
+        Parser innerParser
+
+    let ( <|> ) = orParse
+
+    let choice parserList =
+        List.reduce orParse parserList
+
+    let anyCharOf validChars =
+        validChars
+        |> List.map expectChar
+        |> choice
+
+    let andParse parser1 parser2 =
+        let innerParser inputChars =
+            match runParser parser1 inputChars with
+            | Failure msg -> Failure msg
+            | Success (c1, remaining1) ->
+                match runParser parser2 remaining1 with
+                | Failure msg -> Failure msg
+                | Success (c2, remaining2) ->
+                    Success ((c1, c2), remaining2)
+
+        Parser innerParser
+
+    let ( .>>. ) = andParse
+
+    let mapParser mapFunc parser =
+        let innerParser inputChars =
+            match runParser parser inputChars with
+            | Failure msg -> Failure msg
+            | Success (result, remaining) ->
+                Success (mapFunc result, remaining)
+
+        Parser innerParser
+
+    let applyParser funcAsParser paramAsParser =
+        (funcAsParser .>>. paramAsParser)
+        |> mapParser (fun (f, x) -> f x)
+
+    let ( <*> ) = applyParser
+
+    let returnAsParser result =
+        let innerParser inputChars =
+            Success (result, inputChars)
+
+        Parser innerParser
+
+    let liftToParser2 funcToLift paramAsParser1 paramAsParser2 =
+        returnAsParser funcToLift <*> paramAsParser1 <*> paramAsParser2
+
+    let rec sequenceParsers parserList =
+        let cons head rest = head :: rest
+        let consAsParser = liftToParser2 cons
+
+        match parserList with
+        | [] -> returnAsParser []
+        | parser :: remainingParsers ->
+            consAsParser parser (sequenceParsers remainingParsers)
+
+    let charListAsString chars =
+        System.String(List.toArray chars)
+
+    let expectString expectedString =
+        expectedString
+        |> stringToCharList
+        |> List.map expectChar
+        |> sequenceParsers
+        |> mapParser charListAsString
+
+    stringToCharList "take"
+    |> runParser (expectString "lake" <|> expectString "take")
+    |> printfn "%A"
 
